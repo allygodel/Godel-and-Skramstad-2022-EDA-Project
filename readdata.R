01aded74bc6f697971e509e21e213970f6904916
Allyson Godel <dv4218bx@mnstate.edu>
2022-03-22 11:49:34 -0500

    DataInput

--- readdata.R
+++ readdata.R
@@ -1,3 +1,13 @@
-file1_url <- "https://datacenter.kidscount.org/rawdata.axd?ind=9714&loc=1"
-download.file(file1_url, "data/file1.xlsx")
-data1 <- read_excel("data/file1.xlsx")
+KidsCount1_URL <- "https://datacenter.kidscount.org/rawdata.axd?ind=9714&loc=1"
+download.file(KidsCount1_URL, "data/file1.xlsx")
+InfluenzeSchool <- read.xlsx(KidsCount1_URL)
+
+KidsCount2_URL <- "https://datacenter.kidscount.org/rawdata.axd?ind=10883&loc=1"
+download.file(KidsCount2_URL, "data/file1.xlsx")
+SARSSchool <- read.xlsx(KidsCount2_URL)
+
+CDCSARS <- "https://covidtracking.com/data/download/national-history.csv"
+download.file(CDCSARS, "data/file1.csv")
+SARSTracker <- read.csv(CDCSARS)
+
+
